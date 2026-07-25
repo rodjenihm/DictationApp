87a17600dcd9dbad2481617a2dd085faec910261
Danijel Mitrović <rodjenihm@gmail.com>
2026-07-26 00:15:19 +0200

    Initial Commit

---
+++ DictationApp/DictationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictationAppApp.swift
+//  DictationApp
+//
+//  Created by Danijel Mitrović on 26. 7. 2026..
+//
+
+import SwiftUI
+
+@main
+struct DictationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
